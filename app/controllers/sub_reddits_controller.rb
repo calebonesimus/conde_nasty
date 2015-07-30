15dec7611f1039dc0c4fb42bc6b8fb83db9b43cb
Caleb Francis <calebonesimus@gmail.com>
2015-07-30 14:27:37 -0400

    Link posts and subreddits

--- app/controllers/sub_reddits_controller.rb
+++ app/controllers/sub_reddits_controller.rb
@@ -1,9 +1,11 @@
 class SubRedditsController < ApplicationController
 
   def hot_reddit
+    @posts = SubReddit.find_by("name == ?", "hot_reddit").posts.reverse
   end
 
   def nasty_reddit
+    @posts = SubReddit.find_by("name == ?", "nasty_reddit").posts.reverse
   end
-  
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :sub_reddit
+
+  has_many :up_votes
+  has_many :down_votes
 end
