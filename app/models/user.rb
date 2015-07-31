536f7ecf27b5b89f3ba1222f6046505962fa7396
Caleb Francis <calebonesimus@gmail.com>
2015-07-31 10:55:40 -0400

    Devise works with basic signup/signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :up_votes
+  has_many :down_votes
+  has_many :posts
 end
