47020105f05ab94101404c556eee5e2cee8ff041
Caleb Francis <calebonesimus@gmail.com>
2015-08-02 16:49:16 -0400

    Remove duplicate posts from upvoted/downvoted posts

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,11 +6,21 @@ class ProfilesController < ApplicationController
   def upvotes
     @user = User.find_by "username = ?", params[:username]
     @up_votes = @user.up_votes
+
+    # Returns array of post objects and removes duplicates
+    @posts = @up_votes.collect do |vote|
+      vote.post
+    end.uniq
   end
 
   def downvotes
     @user = User.find_by "username = ?", params[:username]
     @down_votes = @user.down_votes
+
+    # Returns array of post objects and removes duplicates
+    @posts = @down_votes.collect do |vote|
+      vote.post
+    end.uniq
   end
 
 end
