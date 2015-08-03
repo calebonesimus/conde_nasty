774bfdae0c66245f0387e6936de04da56bfb460c
Caleb Francis <calebonesimus@gmail.com>
2015-08-03 13:24:46 -0400

    Condense subreddit controller

--- app/controllers/sub_reddits_controller.rb
+++ app/controllers/sub_reddits_controller.rb
@@ -1,15 +1,22 @@
 class SubRedditsController < ApplicationController
 
-  def hot_reddit
-    @posts = SubReddit.find_by("name = ?", "hot_reddit").posts.sort_by do |post|
+  def show
+    @subreddit = SubReddit.find_by_name(params[:reddit] || "hot_reddit")
+    @posts = @subreddit.posts.sort_by do |post|
       post.final_votes
     end.reverse
   end
 
-  def nasty_reddit
-    @posts = SubReddit.find_by("name = ?", "nasty_reddit").posts.sort_by do |post|
-      post.final_votes
-    end.reverse
-  end
+  # def hot_reddit
+  #   @posts = SubReddit.find_by("name = ?", "hot_reddit").posts.sort_by do |post|
+  #     post.final_votes
+  #   end.reverse
+  # end
+  #
+  # def nasty_reddit
+  #   @posts = SubReddit.find_by("name = ?", "nasty_reddit").posts.sort_by do |post|
+  #     post.final_votes
+  #   end.reverse
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,6 @@ Rails.application.routes.draw do
 
   post 'must_sign_in' => 'posts#must_sign_in', as: :must_sign_in
 
-
-
-  get 'hot_reddit' => 'sub_reddits#hot_reddit', as: :hot_reddit
-  get 'nasty_reddit' => 'sub_reddits#nasty_reddit', as: :nasty_reddit
-  root 'sub_reddits#hot_reddit'
+  get 'r/:reddit' => 'sub_reddits#show', as: :subreddit
+  root 'sub_reddits#show'
 end
